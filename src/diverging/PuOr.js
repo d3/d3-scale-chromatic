150cf8a9fe159dfcd8194934ae466ad3feb7b995
Jeffrey Heer <jeffrey.heer@gmail.com>
2017-02-15 13:32:58 -0800

    Swap order of PuOr scheme.

--- src/diverging/PuOr.js
+++ src/diverging/PuOr.js
@@ -2,15 +2,15 @@ import colors from "../colors";
 import ramp from "../ramp";
 
 export var scheme = new Array(3).concat(
-  "f1a340f7f7f7998ec3",
-  "e66101fdb863b2abd25e3c99",
-  "e66101fdb863f7f7f7b2abd25e3c99",
-  "b35806f1a340fee0b6d8daeb998ec3542788",
-  "b35806f1a340fee0b6f7f7f7d8daeb998ec3542788",
-  "b35806e08214fdb863fee0b6d8daebb2abd28073ac542788",
-  "b35806e08214fdb863fee0b6f7f7f7d8daebb2abd28073ac542788",
-  "7f3b08b35806e08214fdb863fee0b6d8daebb2abd28073ac5427882d004b",
-  "7f3b08b35806e08214fdb863fee0b6f7f7f7d8daebb2abd28073ac5427882d004b"
+  "998ec3f7f7f7f1a340",
+  "5e3c99b2abd2fdb863e66101",
+  "5e3c99b2abd2f7f7f7fdb8630571b0",
+  "542788998ec3d8daebfee0b6f1a340b35806",
+  "542788998ec3d8daebf7f7f7fee0b6f1a340b35806",
+  "5427888073acb2abd2d8daebfee0b6fdb863e08214b35806",
+  "5427888073acb2abd2d8daebf7f7f7fee0b6fdb863e08214b35806",
+  "2d004b5427888073acb2abd2d8daebfee0b6fdb863e08214b358067f3b08",
+  "2d004b5427888073acb2abd2d8daebf7f7f7fee0b6fdb863e08214b358067f3b08"
 ).map(colors);
 
 export default ramp(scheme);
