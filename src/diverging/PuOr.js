d3aa77aa041be150b1e24c8252f1f3c8d45c2a3c
Mike Bostock <mbostock@gmail.com>
2017-02-15 14:18:06 -0800

    Fix transposition.

--- src/diverging/PuOr.js
+++ src/diverging/PuOr.js
@@ -4,7 +4,7 @@ import ramp from "../ramp";
 export var scheme = new Array(3).concat(
   "998ec3f7f7f7f1a340",
   "5e3c99b2abd2fdb863e66101",
-  "5e3c99b2abd2f7f7f7fdb8630571b0",
+  "5e3c99b2abd2f7f7f7fdb863e66101",
   "542788998ec3d8daebfee0b6f1a340b35806",
   "542788998ec3d8daebf7f7f7fee0b6f1a340b35806",
   "5427888073acb2abd2d8daebfee0b6fdb863e08214b35806",
