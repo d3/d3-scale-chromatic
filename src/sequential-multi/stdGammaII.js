259265d43872449c1ff9496e62d4b6e59602fabe
Nicolas Noé <nicolas.noe@inbo.be>
2021-04-16 11:25:18 +0200

    de-reversed Std Gamma II

--- src/sequential-multi/stdGammaII.js
+++ src/sequential-multi/stdGammaII.js
@@ -5,7 +5,7 @@ import {scaleLinear} from "d3-scale";
 // Reimplementation of the IDL "STD Gamma II" color scale, based on bioRad's implementation (https://github.com/adokter/bioRad/blob/e0ede427eb34007dc9985302d40cbdab158e0636/R/color_scale.R#L65-L85)
 // and the explanations at: https://github.com/inbo/crow/issues/38 
 export default function(t) {
-    t = t * 255; // Constants below are taken from bioRad and have a 0-255 range
+    t = 255 - (t * 255); // Constants below are taken from bioRad and have a 0-255 range
   
     return rgb(
         scaleLinear().domain([0, 62, 81, 93, 145, 176, 191, 208, 255]).range([255, 255, 163, 255, 255, 81, 81, 0, 0])(t), 
